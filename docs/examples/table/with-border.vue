d1376e4e465e6ef2691513c0e1bbb6a59bef8653
LandaHao <37289331+LandaHao@users.noreply.github.com>
2023-05-05 17:53:47 +0800

    Update with-border.vue

--- docs/examples/table/with-border.vue
+++ docs/examples/table/with-border.vue
@@ -1,8 +1,8 @@
 <template>
   <el-table :data="tableData" border style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
+    <el-table-column prop="date" label="Date" width="180" show-overflow-tooltip/>
+    <el-table-column prop="name" label="Name" width="180" show-overflow-tooltip />
+    <el-table-column prop="address" label="Address" show-overflow-tooltip/>
   </el-table>
 </template>
 
